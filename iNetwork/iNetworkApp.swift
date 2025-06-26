10a44c45056cafb0c8cb5212f507db8a50bb1d79
mohammadgdz <mhd.gdz@gmail.com>
2025-06-26 18:57:40 +0330

    Initial Commit

---
+++ iNetwork/iNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNetworkApp.swift
+//  iNetwork
+//
+//  Created by Mohammad on 2025-06-26.
+//
+
+import SwiftUI
+
+@main
+struct iNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
